6cd6ded354b06046a8d828b277d6575ab68ab898
Akihiro TAKASE <AkihiroTAKASE@users.noreply.github.com>
2015-05-14 01:30:47 +0900

    Remove debug messages.

--- app/controllers/Uploads.scala
+++ app/controllers/Uploads.scala
@@ -2,7 +2,6 @@ package controllers
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import play.api.Logger
 import play.api.mvc.{RawBuffer, Action, Controller}
 import models.ConcurrentUpload
 
@@ -13,21 +12,15 @@ object Uploads extends Controller {
   }
 
   def testBeforeUpload = Action.async { implicit request =>
-    Logger.info("Received a request: GET")
     ConcurrentUpload.checkExistenceFor(request.queryString) map {
       case true =>
-        Logger.info("Chunk is exist.")
         Ok
       case false =>
-        Logger.info("Chunk is not exist.")
         NotFound
     }
   }
 
   def upload = Action.async { implicit request =>
-    Logger.info("Received a request: POST")
-
-    // Get File Info
     val queryString: Map[String, Seq[String]] = request.queryString
     val currentChunkSize: Int = queryString("resumableCurrentChunkSize").head.toInt
 
@@ -38,18 +31,14 @@ object Uploads extends Controller {
           case Some(bytes: Array[Byte]) =>
             ConcurrentUpload.concatenateFileChunk(queryString, bytes) map {
               case ("done" | "complete") =>
-                Logger.info("Upload succeeded.")
                 Ok
               case "error" =>
-                Logger.error("Upload failed. - Internal Server Error")
                 InternalServerError
             }
           case None =>
-            Logger.error("Upload failed. - Internal Server Error")
             Future { InternalServerError }
         }
       case None =>
-        Logger.error("Upload failed. - BadRequest")
         Future { BadRequest }
     }
   }
