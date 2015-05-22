8586b46e4f4bf163a7ea15845f246e74a75d541d
Akihiro TAKASE <AkihiroTAKASE@users.noreply.github.com>
2015-05-22 22:34:14 +0900

    Redefine "UploadService" as trait "_UploadService"
    Additional changes:
     - "_UploadService" trait has a reference of "_ConcurrentUploader".
     - "testBeforeUpload" function returns BadRequest when the query string is empty or
       null.

--- app/controllers/UploadService.scala
+++ app/controllers/UploadService.scala
@@ -2,34 +2,41 @@ package controllers
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import play.api.mvc.{Action, Controller, RawBuffer}
+import play.api.mvc.{AnyContent, Action, Controller, RawBuffer}
 
-import actor.ConcurrentUploader
+import actor.{_ConcurrentUploader, ConcurrentUploader}
 
-/** A controller which handles some file uploading operations. */
-object UploadService extends Controller {
+trait _UploadService { this: Controller =>
+  val concurrentUploader: _ConcurrentUploader = ConcurrentUploader
 
   /** Generate a file upload form.
-   *
-   * @return Result
-   */
+    *
+    * @return Result
+    */
   def uploadForm = Action { Ok(views.html.uploadForm()) }
 
   /** Check a chunk whether it has already uploaded.
-   *
-   * @return Future[Result]
-   */
+    *
+    * @return Future[Result]
+    */
   def testBeforeUpload = Action.async { implicit request =>
-    ConcurrentUploader.checkExistenceFor(request.queryString) map {
-      case true => Ok
-      case false => NotFound
+    request.queryString match {
+      case qs: Map[String, Seq[String]] =>
+        if (qs.isEmpty) Future(BadRequest)
+        else {
+          concurrentUploader.checkExistenceFor(request.queryString) map {
+            case true => Ok
+            case false => NotFound
+          }
+        }
+      case _ => Future(BadRequest)
     }
   }
 
   /** Upload a chunk.
-   *
-   * @return Future[Result]
-   */
+    *
+    * @return Future[Result]
+    */
   def upload = Action.async { implicit request =>
     val queryString: Map[String, Seq[String]] = request.queryString
     val currentChunkSize: Int = queryString("resumableCurrentChunkSize").head.toInt
@@ -39,7 +46,7 @@ object UploadService extends Controller {
       case Some(raw: RawBuffer) =>
         raw.asBytes(currentChunkSize) match {
           case Some(bytes: Array[Byte]) =>
-            ConcurrentUploader.concatenateFileChunk(queryString, bytes) map {
+            concurrentUploader.concatenateFileChunk(queryString, bytes) map {
               case ("done" | "complete") => Ok
               case "error" => InternalServerError
             }
@@ -49,3 +56,6 @@ object UploadService extends Controller {
     }
   }
 }
+
+/** A controller which handles some file uploading operations. */
+object UploadService extends Controller with _UploadService
