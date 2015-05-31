548c079ccd387c9c6b868513e5dc986d6c6e0ea7
Akihiro TAKASE <AkihiroTAKASE@users.noreply.github.com>
2015-05-31 22:12:14 +0900

    Add some properties to ResumableHelper object
    - chunkFirst
    - chunkSecond
    - chunkLast
    - chunkLengthZero
    - chunkFirstQS
    - chunkSecondQS
    - chunkLastQS
    - chunkLengthZeroQS

--- test/helper/ResumableHelper.scala
+++ test/helper/ResumableHelper.scala
@@ -1,7 +1,9 @@
 package helper
 
+import play.utils.UriEncoding.encodePathSegment
+
 object ResumableHelper {
-  lazy val dummyParams = Map(
+  lazy val chunkFirst = Map(
     "resumableChunkNumber" -> Seq("1"),
     "resumableChunkSize" -> Seq("1048576"),
     "resumableCurrentChunkSize" -> Seq("1048576"),
@@ -11,6 +13,14 @@ object ResumableHelper {
     "resumableFilename" -> Seq("data.zip"),
     "resumableRelativePath" -> Seq("data.zip"),
     "resumableTotalChunks" -> Seq("174"))
+  lazy val chunkSecond = chunkFirst + ("resumableChunkNumber" -> Seq("2"))
+  lazy val chunkLast = chunkFirst + ("resumableChunkNumber" -> Seq("174"))
+  lazy val chunkLengthZero = chunkFirst + ("resumableCurrentChunkSize" -> Seq("0"))
+
+  lazy val chunkFirstQS = (for { (k: String, v: Seq[String]) <- chunkFirst } yield k + "=" + encodePathSegment(v.head, "UTF-8")).mkString("&")
+  lazy val chunkSecondQS = (for { (k: String, v: Seq[String]) <- chunkSecond } yield k + "=" + encodePathSegment(v.head, "UTF-8")).mkString("&")
+  lazy val chunkLastQS = (for { (k: String, v: Seq[String]) <- chunkLast } yield k + "=" + encodePathSegment(v.head, "UTF-8")).mkString("&")
+  lazy val chunkLengthZeroQS = (for { (k: String, v: Seq[String]) <- chunkLengthZero } yield k + "=" + encodePathSegment(v.head, "UTF-8")).mkString("&")
 
   lazy val dummyChunk = {
     val bytes = new Array[Byte](1024 * 1024) // 1 MB
