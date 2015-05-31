95f4d6e26a1e1e6ba38dc704d43ac24e234f9b6b
Akihiro TAKASE <AkihiroTAKASE@users.noreply.github.com>
2015-05-31 22:16:42 +0900

    Reformat test/models/FileSpec.scala

--- test/models/FileSpec.scala
+++ test/models/FileSpec.scala
@@ -3,15 +3,14 @@ package models
 import org.specs2.mutable.Specification
 import play.api.{Application, GlobalSettings}
 import play.api.test.{FakeApplication, WithApplication}
-import play.api.test.Helpers._
+import play.api.test.Helpers.inMemoryDatabase
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
+import dao.FilesDAO
+import models.{File => FileM}
+
 class FileSpec extends Specification {
-
-  import dao.FilesDAO
-  import models.{File => FileM}
-
   def fakeApp = FakeApplication(
     additionalConfiguration = inMemoryDatabase(),
     withGlobal = Some(new GlobalSettings {
@@ -27,25 +26,21 @@ class FileSpec extends Specification {
     })
   )
 
+  def filesDao = new FilesDAO
+
   "File model" should {
-
-    def filesDao = new FilesDAO
-
     "returns tne number of files stored" in new WithApplication(fakeApp) {
       val count = Await.result(filesDao.count(), Duration.Inf)
       count must equalTo(3)
     }
-
     "be retrieved by id" in new WithApplication(fakeApp) {
       val file = Await.result(filesDao.findById(1), Duration.Inf)
       file.name must equalTo("test1.txt")
     }
-
     "be retrieved by name" in new WithApplication(fakeApp) {
       val file = Await.result(filesDao.findByName("test1.txt"), Duration.Inf)
       file.id must equalTo(Some(1))
     }
-
     "be listed" in new WithApplication(fakeApp) {
       val files = Await.result(filesDao.list(), Duration.Inf)
       files.total must equalTo(3)
