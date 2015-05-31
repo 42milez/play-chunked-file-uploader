128d8ba9af1aba5d2e2229c378d5396480bd640e
Akihiro TAKASE <AkihiroTAKASE@users.noreply.github.com>
2015-05-31 22:28:21 +0900

    Reformat test/actors/ConcurrentUploaderSpec.scala

--- test/actors/ConcurrentUploaderSpec.scala
+++ test/actors/ConcurrentUploaderSpec.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration.DurationInt
 import ChunkConcatenatorProtocol.Chunk
 import ConcurrentUploaderProtocol._
 import helper.AkkaHelper.TestEnvironment
-import helper.ResumableHelper.{dummyChunk, dummyParams}
+import helper.ResumableHelper.{dummyChunk, chunkFirst => c1}
 
 class ConcurrentUploaderSpec extends Specification {
   implicit private val timeout: akka.util.Timeout = 1 second
@@ -23,8 +23,8 @@ class ConcurrentUploaderSpec extends Specification {
       "and returns \"false\"" in new TestEnvironment(ActorSystem("TestSystem-01")) {
         new WithApplication {
           val actorRef = system.actorOf(ConcurrentUploader.props, "Supervisor")
-          val actorName = sign(dummyParams("resumableIdentifier").head)
-          val chunkNumber = dummyParams("resumableChunkNumber").head.toInt
+          val actorName = sign(c1("resumableIdentifier").head)
+          val chunkNumber = c1("resumableChunkNumber").head.toInt
           actorRef ! new Test(actorName, chunkNumber)
           expectMsgPF() {
             case result: Boolean => result must equalTo(false)
@@ -37,13 +37,13 @@ class ConcurrentUploaderSpec extends Specification {
           //////////////////////////////
           // upload a chunk
           val actorRef = system.actorOf(ConcurrentUploader.props, "Supervisor")
-          val actorName = sign(dummyParams("resumableIdentifier").head)
-          val chunkNumber = dummyParams("resumableChunkNumber").head.toInt
-          val chunkSize = dummyParams("resumableChunkSize").head.toInt
-          val currentChunkSize = dummyParams("resumableCurrentChunkSize").head.toInt
-          val filename = dummyParams("resumableFilename").head
-          val identifier = dummyParams("resumableIdentifier").head
-          val totalSize = dummyParams("resumableTotalSize").head.toInt
+          val actorName = sign(c1("resumableIdentifier").head)
+          val chunkNumber = c1("resumableChunkNumber").head.toInt
+          val chunkSize = c1("resumableChunkSize").head.toInt
+          val currentChunkSize = c1("resumableCurrentChunkSize").head.toInt
+          val filename = c1("resumableFilename").head
+          val identifier = c1("resumableIdentifier").head
+          val totalSize = c1("resumableTotalSize").head.toInt
           val chunk = Chunk(chunkNumber, chunkSize, currentChunkSize, dummyChunk, filename, identifier, totalSize)
           actorRef ! new Data(actorName, chunk)
 
@@ -59,13 +59,13 @@ class ConcurrentUploaderSpec extends Specification {
     "receives a Data protocol" in new TestEnvironment(ActorSystem("TestSystem-03")) {
       new WithApplication {
         val actorRef = system.actorOf(ConcurrentUploader.props, "Supervisor")
-        val actorName = sign(dummyParams("resumableIdentifier").head)
-        val chunkNumber = dummyParams("resumableChunkNumber").head.toInt
-        val chunkSize = dummyParams("resumableChunkSize").head.toInt
-        val currentChunkSize = dummyParams("resumableCurrentChunkSize").head.toInt
-        val filename = dummyParams("resumableFilename").head
-        val identifier = dummyParams("resumableIdentifier").head
-        val totalSize = dummyParams("resumableTotalSize").head.toInt
+        val actorName = sign(c1("resumableIdentifier").head)
+        val chunkNumber = c1("resumableChunkNumber").head.toInt
+        val chunkSize = c1("resumableChunkSize").head.toInt
+        val currentChunkSize = c1("resumableCurrentChunkSize").head.toInt
+        val filename = c1("resumableFilename").head
+        val identifier = c1("resumableIdentifier").head
+        val totalSize = c1("resumableTotalSize").head.toInt
         val chunk = Chunk(chunkNumber, chunkSize, currentChunkSize, dummyChunk, filename, identifier, totalSize)
         actorRef ! new Data(actorName, chunk)
         expectMsgPF() {
@@ -77,8 +77,8 @@ class ConcurrentUploaderSpec extends Specification {
       "and return a Result protocol" in new TestEnvironment(ActorSystem("TestSystem-04")) {
         new WithApplication {
           val actorRef = system.actorOf(ConcurrentUploader.props, "Supervisor")
-          val actorName = sign(dummyParams("resumableIdentifier").head)
-          val chunkNumber = dummyParams("resumableChunkNumber").head.toInt
+          val actorName = sign(c1("resumableIdentifier").head)
+          val chunkNumber = c1("resumableChunkNumber").head.toInt
           val status = "done"
           actorRef ! new Progress(actorName, status, chunkNumber, self)
           expectMsgPF() {
@@ -92,13 +92,13 @@ class ConcurrentUploaderSpec extends Specification {
           //////////////////////////////
           // upload a chunk
           val actorRef = system.actorOf(ConcurrentUploader.props, "Supervisor")
-          val actorName = sign(dummyParams("resumableIdentifier").head)
-          val chunkNumber = dummyParams("resumableChunkNumber").head.toInt
-          val chunkSize = dummyParams("resumableChunkSize").head.toInt
-          val currentChunkSize = dummyParams("resumableCurrentChunkSize").head.toInt
-          val filename = dummyParams("resumableFilename").head
-          val identifier = dummyParams("resumableIdentifier").head
-          val totalSize = dummyParams("resumableTotalSize").head.toInt
+          val actorName = sign(c1("resumableIdentifier").head)
+          val chunkNumber = c1("resumableChunkNumber").head.toInt
+          val chunkSize = c1("resumableChunkSize").head.toInt
+          val currentChunkSize = c1("resumableCurrentChunkSize").head.toInt
+          val filename = c1("resumableFilename").head
+          val identifier = c1("resumableIdentifier").head
+          val totalSize = c1("resumableTotalSize").head.toInt
           val chunk = Chunk(chunkNumber, chunkSize, currentChunkSize, dummyChunk, filename, identifier, totalSize)
           actorRef ! new Data(actorName, chunk)
 
